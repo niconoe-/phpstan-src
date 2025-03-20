c6e045060f11ec49c58327f34cbff370baff30d2
Markus Staab <markus.staab@redaxo.de>
2025-03-20 13:53:02 +0100

    Fix `count()` regression

--- src/Analyser/TypeSpecifier.php
+++ src/Analyser/TypeSpecifier.php
@@ -1063,10 +1063,12 @@ final class TypeSpecifier
 
 		$isConstantArray = $type->isConstantArray();
 		$isList = $type->isList();
+		$zeroOrMore = IntegerRangeType::fromInterval(0, null);
 		if (
 			!$isNormalCount->yes()
 			|| (!$isConstantArray->yes() && !$isList->yes())
 			|| $type->isIterableAtLeastOnce()->no() // array{} cannot be used for further narrowing
+			|| !$zeroOrMore->isSuperTypeOf($sizeType)->yes()
 		) {
 			return null;
 		}
