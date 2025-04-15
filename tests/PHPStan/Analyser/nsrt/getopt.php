99299a27f407afe34c2ef8037c84bbe4a6f90e9b
Jan Nedbal <janedbal@gmail.com>
2025-04-15 14:21:58 +0200

    Improve `getopt()` function stub

--- tests/PHPStan/Analyser/nsrt/getopt.php
+++ tests/PHPStan/Analyser/nsrt/getopt.php
@@ -5,5 +5,6 @@ namespace Getopt;
 use function getopt;
 use function PHPStan\Testing\assertType;
 
-$opts = getopt("ab:c::", ["longopt1", "longopt2:", "longopt3::"]);
+$opts = getopt("ab:c::", ["longopt1", "longopt2:", "longopt3::"], $restIndex);
 assertType('(array<string, list<mixed>|string|false>|false)', $opts);
+assertType('int<1, max>', $restIndex);
