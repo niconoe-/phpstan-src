c7f870adbb2cf29073d86977066398bfe0516624
Ondrej Mirtes <ondrej@mirtes.cz>
2025-04-19 14:16:09 +0200

    Fix text

--- tests/PHPStan/Analyser/nsrt/bug-5017.php
+++ tests/PHPStan/Analyser/nsrt/bug-5017.php
@@ -15,7 +15,7 @@ class Foo
 			assertType('non-empty-array<0|1|2|3|4, 0|1|2|3|4>', $items);
 			$batch = array_splice($items, 0, 2);
 			assertType('array<0|1|2|3|4, 0|1|2|3|4>', $items);
-			assertType('non-empty-array<0|1|2|3|4, 0|1|2|3|4>', $batch);
+			assertType('non-empty-list<0|1|2|3|4>', $batch);
 		}
 	}
 
