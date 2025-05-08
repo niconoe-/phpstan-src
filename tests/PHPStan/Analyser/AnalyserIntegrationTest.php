a12642a0bf0b0c0480771fa9c858ca6f1ecf487f
Andrei Ivchenkov <a.ivchenkov@volta.space>
2025-05-08 13:49:18 +0300

    Fix callable-string must be non-empty-string

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -1487,6 +1487,12 @@ class AnalyserIntegrationTest extends PHPStanTestCase
 		$this->assertNoErrors($errors);
 	}
 
+	public function testBug12979(): void
+	{
+		$errors = $this->runAnalyse(__DIR__ . '/data/bug-12979.php');
+		$this->assertNoErrors($errors);
+	}
+
 	/**
 	 * @param string[]|null $allAnalysedFiles
 	 * @return Error[]
