aaa1424c0f90fffc425c397219b26104a99e9231
Ondrej Mirtes <ondrej@mirtes.cz>
2025-05-16 14:20:54 +0200

    Remove unused method from PathNotFoundException

--- src/File/PathNotFoundException.php
+++ src/File/PathNotFoundException.php
@@ -8,14 +8,9 @@ use function sprintf;
 final class PathNotFoundException extends Exception
 {
 
-	public function __construct(private string $path)
+	public function __construct(string $path)
 	{
 		parent::__construct(sprintf('Path %s does not exist', $path));
-	}
-
-	public function getPath(): string
-	{
-		return $this->path;
 	}
 
 }
