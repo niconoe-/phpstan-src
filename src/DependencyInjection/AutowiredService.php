339a29d3d0480b7c145e2d49127ae2b442c22daf
Ondrej Mirtes <ondrej@mirtes.cz>
2025-05-25 09:05:23 +0200

    Explaining doc comment

--- src/DependencyInjection/AutowiredService.php
+++ src/DependencyInjection/AutowiredService.php
@@ -4,6 +4,14 @@ namespace PHPStan\DependencyInjection;
 
 use Attribute;
 
+/**
+ * Registers a service in the DI container.
+ *
+ * Auto-adds service extension tags based on implemented interfaces.
+ *
+ * Works thanks to https://github.com/ondrejmirtes/composer-attribute-collector
+ * and AutowiredAttributeServicesExtension.
+ */
 #[Attribute(flags: Attribute::TARGET_CLASS)]
 final class AutowiredService
 {
