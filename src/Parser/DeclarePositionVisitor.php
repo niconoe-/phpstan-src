ded58e8a33974033005a6acede400745dbef89fc
Ondrej Mirtes <ondrej@mirtes.cz>
2025-05-26 11:01:32 +0200

    More AutowiredService

--- src/Parser/DeclarePositionVisitor.php
+++ src/Parser/DeclarePositionVisitor.php
@@ -4,8 +4,10 @@ namespace PHPStan\Parser;
 
 use PhpParser\Node;
 use PhpParser\NodeVisitorAbstract;
+use PHPStan\DependencyInjection\AutowiredService;
 use function str_starts_with;
 
+#[AutowiredService]
 final class DeclarePositionVisitor extends NodeVisitorAbstract
 {
 
