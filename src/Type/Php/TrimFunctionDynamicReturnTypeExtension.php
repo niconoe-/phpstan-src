60543dac2e3e9c3ca412f69ef885f62ab165cab7
Ondrej Mirtes <ondrej@mirtes.cz>
2025-05-26 11:25:24 +0200

    TrimFunctionDynamicReturnTypeExtension - register as AutowiredService

--- src/Type/Php/TrimFunctionDynamicReturnTypeExtension.php
+++ src/Type/Php/TrimFunctionDynamicReturnTypeExtension.php
@@ -4,6 +4,7 @@ namespace PHPStan\Type\Php;
 
 use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
+use PHPStan\DependencyInjection\AutowiredService;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Type\Accessory\AccessoryLowercaseStringType;
 use PHPStan\Type\Accessory\AccessoryUppercaseStringType;
@@ -14,6 +15,7 @@ use PHPStan\Type\Type;
 use function count;
 use function in_array;
 
+#[AutowiredService]
 final class TrimFunctionDynamicReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
 
