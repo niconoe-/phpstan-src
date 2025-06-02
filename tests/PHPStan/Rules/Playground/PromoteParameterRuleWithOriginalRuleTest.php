5c22b9d6a65aba28da96b0780b94d58630eb1b1f
Ondrej Mirtes <ondrej@mirtes.cz>
2025-06-02 16:43:25 +0200

    PromoteParameterRule - deduplicate already reported errors

---
+++ tests/PHPStan/Rules/Playground/PromoteParameterRuleWithOriginalRuleTest.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Playground;
+
+use PHPStan\Node\InClassMethodNode;
+use PHPStan\Php\PhpVersion;
+use PHPStan\Reflection\Php\PhpClassReflectionExtension;
+use PHPStan\Rules\Methods\MethodParameterComparisonHelper;
+use PHPStan\Rules\Methods\MethodSignatureRule;
+use PHPStan\Rules\Methods\MethodVisibilityComparisonHelper;
+use PHPStan\Rules\Methods\OverridingMethodRule;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+
+/**
+ * @extends RuleTestCase<PromoteParameterRule<InClassMethodNode>>
+ */
+class PromoteParameterRuleWithOriginalRuleTest extends RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return new PromoteParameterRule(
+			new OverridingMethodRule(
+				self::getContainer()->getByType(PhpVersion::class),
+				self::getContainer()->getByType(MethodSignatureRule::class),
+				true,
+				self::getContainer()->getByType(MethodParameterComparisonHelper::class),
+				self::getContainer()->getByType(MethodVisibilityComparisonHelper::class),
+				self::getContainer()->getByType(PhpClassReflectionExtension::class),
+				true,
+			),
+			self::getContainer(),
+			InClassMethodNode::class,
+			false,
+			'checkMissingOverrideMethodAttribute',
+		);
+	}
+
+	public function testRule(): void
+	{
+		$this->analyse([__DIR__ . '/data/promote-missing-override.php'], [
+			[
+				'Method PromoteMissingOverride\Bar::doFoo() overrides method PromoteMissingOverride\Foo::doFoo() but is missing the #[\Override] attribute.',
+				18,
+				'This error would be reported if the <fg=cyan>checkMissingOverrideMethodAttribute: true</> parameter was enabled in your <fg=cyan>%configurationFile%</>.',
+			],
+		]);
+	}
+
+}
---
+++ tests/PHPStan/Rules/Playground/data/promote-missing-override.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PromoteMissingOverride;
+
+class Foo
+{
+
+	public function doFoo(): void
+	{
+
+	}
+
+}
+
+class Bar extends Foo
+{
+
+	public function doFoo(): void
+	{
+
+	}
+
+
+	public function doBar(): void
+	{
+
+	}
+
+	#[\Override]
+	public function doBaz(): void
+	{
+	}
+
+}
