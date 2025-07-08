400ba6d71ae15f52c923b5251114839261b49a27
Ondrej Mirtes <ondrej@mirtes.cz>
2025-07-08 09:33:22 +0200

    Fix infinite recursion with TemplateIterableType

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -451,6 +451,16 @@ class AnalyserIntegrationTest extends PHPStanTestCase
 		$this->assertNotEmpty($errors);
 	}
 
+	public function testBug13218(): void
+	{
+		if (PHP_VERSION_ID < 80000) {
+			$this->markTestSkipped('Test requires PHP 8.0.');
+		}
+
+		$errors = $this->runAnalyse(__DIR__ . '/data/bug-13218.php');
+		$this->assertNoErrors($errors);
+	}
+
 	public function testBug5529(): void
 	{
 		$errors = $this->runAnalyse(__DIR__ . '/nsrt/bug-5529.php');
