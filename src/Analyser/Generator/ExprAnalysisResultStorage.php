35005471c8bb181a808f62ce1a95fd71ba08d1ae
Ondrej Mirtes <ondrej@mirtes.cz>
2025-11-13 14:21:36 +0100

    Add `ExprAnalysisResultStorage::getExprAnalysisResult()`

--- src/Analyser/Generator/ExprAnalysisResultStorage.php
+++ src/Analyser/Generator/ExprAnalysisResultStorage.php
@@ -3,7 +3,10 @@
 namespace PHPStan\Analyser\Generator;
 
 use PhpParser\Node\Expr;
+use PHPStan\ShouldNotHappenException;
 use SplObjectStorage;
+use function get_class;
+use function sprintf;
 
 final class ExprAnalysisResultStorage
 {
@@ -24,6 +27,15 @@ final class ExprAnalysisResultStorage
 	public function findExprAnalysisResult(Expr $expr): ?ExprAnalysisResult
 	{
 		return $this->expressionAnalysisResults[$expr] ?? null;
+	}
+
+	public function getExprAnalysisResult(Expr $expr): ExprAnalysisResult
+	{
+		if (!isset($this->expressionAnalysisResults[$expr])) {
+			throw new ShouldNotHappenException(sprintf('Result for expr %s not found', get_class($expr)));
+		}
+
+		return $this->expressionAnalysisResults[$expr];
 	}
 
 }
